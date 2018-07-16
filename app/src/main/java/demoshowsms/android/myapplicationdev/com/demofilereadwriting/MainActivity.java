5167a9852a0c281da9c33602c54735d8f01adf25
16022653 <16022653@myrp.edu.sg>
2018-07-16 15:01:44 +0800

    Initial commit

---
+++ app/src/main/java/demoshowsms/android/myapplicationdev/com/demofilereadwriting/MainActivity.java
@@ -0,0 +1,93 @@
+package demoshowsms.android.myapplicationdev.com.demofilereadwriting;
+
+import android.os.Environment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+
+public class MainActivity extends AppCompatActivity {
+
+    String folderLocation;
+    Button btnWrite, btnRead;
+    TextView txt;
+//UI handlers to be defined
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //UI handlers to be defined
+        btnRead = (Button) findViewById(R.id.button2);
+        btnWrite = (Button) findViewById(R.id.button);
+        txt = (TextView)findViewById(R.id.textView);
+
+        folderLocation = Environment.getExternalStorageDirectory().getAbsolutePath() + "/MyFolder";
+
+        File folder = new File(folderLocation);
+        if (folder.exists() == false) {
+            boolean result = folder.mkdir();
+            if (result == true) {
+                Log.d("File Read/Write", "Folder created");
+            }
+        }
+
+        btnWrite.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    String folderLocation = Environment.getExternalStorageDirectory().getAbsolutePath() + "/Folder";
+                    File targetFile = new File(folderLocation, "data.txt");
+                    FileWriter writer = new FileWriter(targetFile, true);
+                    writer.write("Hello world" + "\n");
+                    writer.flush();
+                    writer.close();
+                } catch (Exception e) {
+                    Toast.makeText(MainActivity.this, "Failed to write!", Toast.LENGTH_LONG).show();
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        btnRead.setOnClickListener(new View.OnClickListener()
+
+        {
+            @Override
+            public void onClick(View v) {
+                String folderLocation = Environment.getExternalStorageDirectory().getAbsolutePath() + "/Folder";
+                File targetFile = new File(folderLocation, "data.txt");
+                if (targetFile.exists() == true) {
+                    String data = "";
+                    try {
+                        FileReader reader = new FileReader(targetFile);
+                        BufferedReader br = new BufferedReader(reader);
+                        String line = br.readLine();
+                        while (line != null) {
+                            data += line + "\n";
+                            line = br.readLine();
+
+                        }
+                        txt.setText(data);
+                        br.close();
+                        reader.close();
+                    } catch (Exception e) {
+                        Toast.makeText(MainActivity.this, "Failed to read!", Toast.LENGTH_LONG).show();
+                        e.printStackTrace();
+                    }
+                    Log.d("Content", data);
+                }
+            }
+        });
+
+
+    }
+}
